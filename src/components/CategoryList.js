9a0a085c730c768a4e23da68d6c279f205c5e0f0
Ali Emir Şahin <aliemir.455687@gmail.com>
2025-02-24 16:23:49 +0300

    updating app.js

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -38,7 +38,7 @@ export default class CategoryList extends Component {
             </li>
           ))}
         </ul>
-        <h4>{this.props.currentCategory}</h4>
+        {/* <h4>{this.props.currentCategory}</h4> */}
       </div>
     );
   }
