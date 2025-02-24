b89298f8d37218ed937d633d957ec53485fd234e
Ali Emir Şahin <aliemir.455687@gmail.com>
2025-02-24 16:54:34 +0300

    design improved

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -2,16 +2,11 @@ import React, { Component } from 'react';
 import "./../styles/Category.css";
 
 export default class CategoryList extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      categories: [
-        { categoryId: 1, categoryName: "Electronic" },
-        { categoryId: 2, categoryName: "Clothes" },
-        { categoryId: 3, categoryName: "Home & Garden" },
-        { categoryId: 4, categoryName: "Books" }
-      ],
+      categories: [],
+      isCategoriesVisible: false, 
     };
   }
 
@@ -19,26 +14,38 @@ export default class CategoryList extends Component {
     this.getCategories();
   }
 
-   getCategories = () => {
+  getCategories = () => {
     fetch("http://localhost:3000/categories")
       .then(response => response.json())
       .then(data => this.setState({ categories: data }));
+  };
 
-   };
+  toggleCategories = () => {
+    this.setState((prevState) => ({
+      isCategoriesVisible: !prevState.isCategoriesVisible
+    }));
+  };
 
   render() {
     return (
       <div className="category-list">
-        <h2>{this.props.info.title}</h2>
-        <ul>
-          {this.state.categories.map(category => (
-            <li onClick={() => this.props.changeCategory(category)} 
-                key={category.id}>
-              {category.categoryName}
-            </li>
-          ))}
-        </ul>
-        {/* <h4>{this.props.currentCategory}</h4> */}
+        <h2 className="category-header" onClick={this.toggleCategories}>
+          {this.props.info.title}
+          <span className="arrow">{this.state.isCategoriesVisible ? "▲" : "▼"}</span>
+        </h2>
+
+        <div className={`category-items ${this.state.isCategoriesVisible ? 'open' : ''}`}>
+          <ul>
+            {this.state.categories.map(category => (
+              <li 
+                onClick={() => this.props.changeCategory(category)} 
+                key={category.id}
+              >
+                {category.categoryName}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
