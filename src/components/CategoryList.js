2667a8cc4c02c7ea0d5d31c86ce4dd1c2d07af4a
Ali Emir Şahin <aliemir.455687@gmail.com>
2025-02-24 12:16:18 +0300

    added eventlistener

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import "./../styles/Category.css";
 
 export default class CategoryList extends Component {
@@ -11,9 +11,15 @@ export default class CategoryList extends Component {
         { categoryId: 2, categoryName: "Giyim" },
         { categoryId: 3, categoryName: "Ev & Bahçe" },
         { categoryId: 4, categoryName: "Kitap" }
-      ]
-    }
+      ],
+      currentCategory: null
+    };
   }
+
+  // Arrow function kullanarak 'this' bağlamını otomatik olarak ayarlıyoruz
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
+  };
 
   render() {
     return (
@@ -21,9 +27,13 @@ export default class CategoryList extends Component {
         <h2>{this.props.info.title}</h2>
         <ul>
           {this.state.categories.map(category => (
-            <li key={category.categoryId}>{category.categoryName}</li>
+            <li onClick={() => this.changeCategory(category)} 
+                key={category.categoryId}>
+              {category.categoryName}
+            </li>
           ))}
         </ul>
+        <h4>{this.state.currentCategory}</h4>
       </div>
     );
   }
