cf7c0d0a7dff12970638190f6ae959f781a20023
Ali Emir Şahin <aliemir.455687@gmail.com>
2025-02-24 11:50:22 +0300

    state structure added

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -3,21 +3,26 @@ import "./../styles/Category.css";
 
 export default class CategoryList extends Component {
 
-constructor(props) {
-  super(props); 
-  state:{}
-
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        { categoryId: 1, categoryName: "Elektronik" },
+        { categoryId: 2, categoryName: "Giyim" },
+        { categoryId: 3, categoryName: "Ev & Bahçe" },
+        { categoryId: 4, categoryName: "Kitap" }
+      ]
+    }
+  }
 
   render() {
     return (
       <div className="category-list">
         <h2>{this.props.info.title}</h2>
         <ul>
-          <li>Elektronik</li>
-          <li>Giyim</li>
-          <li>Ev & Bahçe</li>
-          <li>Kitap</li>
+          {this.state.categories.map(category => (
+            <li key={category.categoryId}>{category.categoryName}</li>
+          ))}
         </ul>
       </div>
     );
