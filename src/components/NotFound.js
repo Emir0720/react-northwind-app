7b8932a2e95af9457ff8d54513dbfebc3a114d1e
Ali Emir Şahin <aliemir.455687@gmail.com>
2025-03-01 00:13:16 +0300

    not found page style added

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import "./../styles/NotFound.css";
 
 export default class NotFound extends Component {
   render() {
     return (
-      <div>
-        Not Found !
+      <div className='container'>
+        <div className="error-code">404</div>
+        <div className="error-message">Oops! Page Not Found</div>
+        <button className="home-button" 
+        onClick={() => window.location.href = '/'}>Go Home</button>
       </div>
-    )
+    );
   }
 }
